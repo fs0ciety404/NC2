24377f72984205ed21caff4c2dc84b2614133864
fs0ciety404 <113516391+fs0ciety404@users.noreply.github.com>
2022-12-07 14:06:48 +0100

    Initial Commit

---
+++ NC2/NC2App.swift
@@ -0,0 +1,17 @@
+//
+//  NC2App.swift
+//  NC2
+//
+//  Created by Davide Ragosta on 07/12/22.
+//
+
+import SwiftUI
+
+@main
+struct NC2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
