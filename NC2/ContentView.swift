ee38fde924edf180e26452979da4bcfa17938cf5
Pasqualecut <pcutolo22@fed.idserve.net>
2022-12-14 14:46:55 +0100

    Per Pasquale

--- NC2/ContentView.swift
+++ NC2/ContentView.swift
@@ -1,35 +1,18 @@
-//
-//  ContentView.swift
-//  NC2
-//
-//  Created by Davide Ragosta on 07/12/22.
-//
-
 import SwiftUI
 import SpriteKit
 
 struct SkView: UIViewRepresentable {
     func makeUIView(context: Context) -> SKView {
         let skView = SKView()
-
+        
         // load up the animation frames
         let textureAtlas = SKTextureAtlas(named: "Sprites")
-        var animationFrames: [SKTexture] = []
-        let frames = [SKTexture(imageNamed: "Stork_0"),
-                      SKTexture(imageNamed: "Stork_1"),
-                      SKTexture(imageNamed: "Stork_2"),
-                      SKTexture(imageNamed: "Stork_3"),
-                      SKTexture(imageNamed: "Stork_4"),
-                      SKTexture(imageNamed: "Stork_5"),
-                      SKTexture(imageNamed: "Stork_6")]
-
-        animationFrames.append(contentsOf: frames)
-
-        for frame in animationFrames {
-            print(frame)
+        let animationFrames = textureAtlas.textureNames.map {
+            SKTexture(imageNamed: $0)
         }
+
         // create the sprite node and add it to the sprite kit view
-        let spriteNode = SKSpriteNode(texture: frames[0])
+        let spriteNode = SKSpriteNode(texture: animationFrames[0])
         skView.presentScene(SKScene(size: spriteNode.size))
         skView.scene?.addChild(spriteNode)
 
@@ -44,10 +27,9 @@ struct SkView: UIViewRepresentable {
         // update the SKView if necessary
     }
 }
+
 struct SkView_Previews: PreviewProvider {
     static var previews: some View {
         SkView()
     }
 }
-
-
